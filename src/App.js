be096b91c618fe82ef71fcba8c3ea6fb8e679845
Julia Darwish <darwishjulia4@gmail.com>
2024-02-01 12:12:38 -0500

    Installed Bootstrap

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import Body from "./Components/Body/Body";
 import Footer from "./Components/Footer/Footer";
-// import { BrowserRouter, Route, Switch } from 'react-router-dom';
-// import ContactUs from "./pages/ContactUs";
-// import Home from "./pages/Home";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import ContactUs from "./pages/ContactUs";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
@@ -12,6 +14,7 @@ const App = () => {
       <Navbar />
       <Body />
       <Footer />
+      
     </>
   );
 };
